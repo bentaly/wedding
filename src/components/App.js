05d876efe86878ea25d0eeba07c5594f608ae7c3
ben taliadoros <bentaliadoros@bens-mbp.home>
2018-09-02 19:55:08 +0100

    bit of style, responsive

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,61 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import RoutingComponent from './RoutingComponent';
+import Navigation from './Navigation';
+import Home from './Home';
+import RSVP from './Rsvp';
+import Accomodation from './Accomodation';
+import Travel from './Travel';
+import GiftRegistry from './GiftRegistry';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.routes = [
+      {
+        label: 'The Big Day',
+        route: '/',
+        component: Home
+      },
+      {
+        label: 'RSVP',
+        route: '/rsvp',
+        component: RSVP
+      },
+      {
+        label: 'Accomodation',
+        route: '/accomodation',
+        component: Accomodation
+      },
+      {
+        label: 'Travel',
+        route: '/travel',
+        component: Travel
+      },
+      {
+        label: 'Gift Registry',
+        route: '/giveusstuff',
+        component: GiftRegistry
+      }
+    ];
+  }
+
   render() {
     return (
-      <div className="app">
-        <header className="app-header">
-          <h1 className="app-title">
-            <span>Camila</span>
-            <span>and</span>
-            <span>Ben</span>
-          </h1>
-        </header>
-        <BrowserRouter>
-          <RoutingComponent />
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <header className="app-header">
+            <h1 className="app-title">
+              <span>Camila</span>
+              <span>and</span>
+              <span>Benjamin</span>
+            </h1>
+            <Navigation routes={this.routes} />
+          </header>
+          <RoutingComponent routes={this.routes} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import './Navigation.css';
+import { Link } from 'react-router-dom';
 
 class Navigation extends Component {
   render() {
-    return <div className="navigation-container">{this.props.children}</div>;
+    return (
+      <div className="navigation-container">
+        {this.props.routes.map(route => (
+          <nav key={route.route}>
+            <Link to={route.route}>{route.label}</Link>
+          </nav>
+        ))}
+      </div>
+    );
   }
 }
 
