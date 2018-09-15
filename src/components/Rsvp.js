e0dc07b1c6b4f0f81014e2506d8cc667079ccf5f
ben taliadoros <bentaliadoros@bens-MacBook-Pro.local>
2018-09-15 10:43:23 +0100

    select for diet

--- src/components/Rsvp.js
+++ src/components/Rsvp.js
@@ -1,8 +1,54 @@
 import React, { Component } from 'react';
 
 class RSVP extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.dietaryOptions = [
+      'Normal people',
+      'Veggie',
+      'Vegan',
+      'Gluten free',
+      'Other (please specify)'
+    ];
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
-    return <div>RSVP</div>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Food:
+          <select onChange={this.handleChange}>
+            {this.dietaryOptions.map(diet => (
+              <option key={diet} value={diet}>
+                {diet}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
