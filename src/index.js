a104c89b5ec5c1a24dce4cf31f214e21db05c71a
ben taliadoros <bentaliadoros@bens-mbp.home>
2018-09-25 22:36:01 +0100

    locale working bisshhhhhhhhhhh!

--- src/index.js
+++ src/index.js
@@ -3,9 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import SiteContainer from './components/SiteContainer';
 import { IntlProvider, addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import pt from 'react-intl/locale-data/pt';
+import localeData from './../build/locales/data.json';
+
+addLocaleData([...en, ...pt]);
+
+// Define user's language. Different browsers have the user locale defined
+// on different fields on the `navigator` object, so we make sure to account
+// for these different by checking all of them
+const language =
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage;
+
+// Split locales with a region code
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+// Try full locale, try locale without region code, fallback to 'en'
+const messages =
+  localeData[languageWithoutRegionCode] ||
+  localeData[language] ||
+  localeData.en;
 
 ReactDOM.render(
-  <IntlProvider>
+  <IntlProvider locale={language} messages={messages}>
     <SiteContainer />
   </IntlProvider>,
   document.getElementById('root')
